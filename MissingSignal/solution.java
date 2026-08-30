21655b22bb1ebc0b52c40aef26883d4675293ba5
purvisonthalia05 <purvisonthalia05@gmail.com>
2026-08-30 15:17:34 +0530

    Update solution.java

--- MissingSignal/solution.java
+++ MissingSignal/solution.java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int xor = n;
+
+        for (int i = 0; i < n; i++) {
+            xor ^= i;
+            xor ^= Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(xor);
+    }
+}
 
