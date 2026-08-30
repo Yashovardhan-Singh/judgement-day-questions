22dcb3aa0c8cdcec9ef2744c01d3d2446e6e9a68
purvisonthalia05 <purvisonthalia05@gmail.com>
2026-08-30 15:33:07 +0530

    Update solution.java

--- BalancedLogStatement/solution.java
+++ BalancedLogStatement/solution.java
@@ -1,31 +1,29 @@
-import java.io.*;
 import java.util.*;
 
-public class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine().trim());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+class Solution {
+    public int longestBalancedSegment(int[] log) {
         HashMap<Integer, Integer> firstSeen = new HashMap<>();
         firstSeen.put(0, -1);
-
         int balance = 0;
-        int answer = 0;
+        int maxLength = 0;
 
-        for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(st.nextToken());
-
-            balance += (x == 1) ? 1 : -1;
+        for (int i = 0; i < log.length; i++) {
+            if (log[i] == 1) {
+                balance++;
+            } else {
+                balance--;
+            }
 
             if (firstSeen.containsKey(balance)) {
-                answer = Math.max(answer, i - firstSeen.get(balance));
+                maxLength = Math.max(
+                    maxLength,
+                    i - firstSeen.get(balance)
+                );
             } else {
                 firstSeen.put(balance, i);
             }
         }
 
-        System.out.println(answer);
+        return maxLength;
     }
 }
