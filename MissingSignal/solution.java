927d06cf3e737e72eb985db1030473f14d5b9676
purvisonthalia05 <purvisonthalia05@gmail.com>
2026-08-30 15:37:19 +0530

    Update solution.java

--- MissingSignal/solution.java
+++ MissingSignal/solution.java
@@ -1,11 +1,19 @@
-class Solution {
-    public int missingSignal(int[] signals) {
-        int n = signals.length;
-        int xor = n;
-        for (int i = 0; i < n; i++) {
-            xor ^= i;
-            xor ^= signals[i];
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int xor = n;
+            for (int i = 0; i < n; i++) {
+                xor ^= i;
+                xor ^= sc.nextInt();
+            }
+            System.out.println(xor);
         }
-        return xor;
+        sc.close();
     }
 }
