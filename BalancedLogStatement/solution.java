f29a04ad50538a28b340c556232f39f332bc69a1
purvisonthalia05 <purvisonthalia05@gmail.com>
2026-08-30 15:38:39 +0530

    Update solution.java

--- BalancedLogStatement/solution.java
+++ BalancedLogStatement/solution.java
@@ -1,29 +1,27 @@
 import java.util.*;
 
-class Solution {
-    public int longestBalancedSegment(int[] log) {
-        HashMap<Integer, Integer> firstSeen = new HashMap<>();
-        firstSeen.put(0, -1);
-        int balance = 0;
-        int maxLength = 0;
-
-        for (int i = 0; i < log.length; i++) {
-            if (log[i] == 1) {
-                balance++;
-            } else {
-                balance--;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            HashMap<Integer, Integer> firstSeen = new HashMap<>();
+            firstSeen.put(0, -1);
+            int balance = 0, answer = 0;
+            for (int i = 0; i < n; i++) {
+                int value = sc.nextInt();
+                if (value == 1) 
+                    balance++;
+                else
+                    balance--;
+                if (firstSeen.containsKey(balance))
+                    answer = Math.max(answer, i - firstSeen.get(balance));
+                else 
+                    firstSeen.put(balance, i);
             }
-
-            if (firstSeen.containsKey(balance)) {
-                maxLength = Math.max(
-                    maxLength,
-                    i - firstSeen.get(balance)
-                );
-            } else {
-                firstSeen.put(balance, i);
-            }
+            System.out.println(answer);
         }
-
-        return maxLength;
+        sc.close();
     }
 }
